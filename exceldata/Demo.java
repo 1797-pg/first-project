20228d38eb4eafbbe1c4d0a72ee9ba7d01090104
Pooja <82363034+1797-pg@users.noreply.github.com>
2021-06-11 14:55:29 +0530

    Update Demo.java

--- exceldata/Demo.java
+++ exceldata/Demo.java
@@ -3,17 +3,19 @@ package com.te.exceldata;
 public class Demo {
 
 	public static void main(String[] args) {
+     //code to reverse the string by words
 		
-//		String str="I am From Test Yantra";
-//		String temp;
-////		int i=str.length()-1;
-////		int start,end=i+1;
-//		String[] str1= str.split(" ") ;
-////		 System.out.println(str1[0]);
-//		for(int i=str1.length-1;i>=0;i--) {
-//			System.out.println(str1[i]);
-//		}
-//		}
+		String str="I am From Test Yantra";
+		String temp;
+		int i=str.length()-1;
+		int start,end=i+1;
+		String[] str1= str.split(" ") ;
+		 System.out.println(str1[0]);
+		for(int i=str1.length-1;i>=0;i--) {
+			System.out.println(str1[i]);
+		}
+		}
+	
 	//code to check pallindrome string	
 	  String str2="malayalam";
 	  int i=0;
